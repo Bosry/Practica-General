7c7ca752f68d9bf7cbbbf0ba065f16f343e1158c
Bosry <b0sry2020xd@gmai.com>
2024-10-21 13:00:41 -0500

    Practica General

--- src/Views/View.java
+++ src/Views/View.java
@@ -1,5 +1,42 @@
 package Views;
 
+import java.util.Scanner;
+
+import Models.Person;
+
 public class View {
-    
+    private Scanner scanner;
+
+    public View(){
+        scanner = new Scanner(System.in);
+    }
+    public int showMenu(){
+        System.out.println("Menu");
+        System.out.println("1. Ingresar Personas");
+
+        System.out.println("100. Salir");
+        System.out.print("Ingrese una opcion: ");
+        return scanner.nextInt();
+    }
+
+    public int inputInt(String message){
+        System.out.println(message);
+        return scanner.nextInt();
+    }
+
+    public Person inputPerson(){
+        String name = inputName();
+        int age = inputAge();
+        return new Person(name, age);
+    }
+
+    public String inputName(){
+        System.out.println("Ingrese e nombre: ");
+        return scanner.next();
+    }
+
+    public int inputAge(){
+        return inputInt("Ingrese la edad: ");
+    }
+   
 }
